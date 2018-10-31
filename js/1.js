6f5922a705e9d04addb4c6b687548fe3b12b508e
jackertran98 <43662575+jackertran98@users.noreply.github.com>
2018-11-01 03:14:53 +0700

    first commit

---
+++ js/1.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+    $('.gt').click(function(){
+        $('.gioithieu').addClass('gt_active');
+        $('.buss').addClass('buss_active');
+        $('.khoibe').remove();
+        $('.khoibe1').addClass('khoibe1_active');
+        $( '.buss' ).addClass('best_active');
+    });
+    $('.khoibe1').click(function(){
+        $('div').removeClass('gt_active');
+        $('div').removeClass('khoibe1_active');
+        $('div').removeClass('buss_active');
+
+    });
+    $('.khoibe').click(function(){
+        $('.gioithieu').addClass('gt_active');
+        $('.buss').addClass('buss_active');
+        $('.khoibe').remove();
+        $('.khoibe1').addClass('khoibe1_active');
+    });
+    $('#zoom').click(function(){
+        $('#zoom').addClass('phongto');
+    });
+    $('.nut ul li a').click(function(){
+        var subject=$(this).data('web');
+        $('.noidung .nd').each(function(){
+            if($(this).hasClass(subject)){
+                $(this).fadeIn("slow");
+            }
+            else{
+                $(this).fadeOut();
+            }
+        })
+        return false;
+    });
+});
+// $(function(){
+	
+// });
+new WOW().init();
+
+
